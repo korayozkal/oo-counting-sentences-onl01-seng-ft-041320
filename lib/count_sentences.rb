47f8bdd9132fefca1a6cedb8116d507eac27c3f1
Koray Ozkal <koryozkal@gmail.com>
2020-04-25 22:41:58 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,8 +28,10 @@ end
   end
 
   def count_sentences
-    self.count_sentences
-    puts 
+    self.count_sentences(nums)
+    self.count_sentences.split 
+    
+    puts "This is a string! It has #{nums} sentences. Right?"
 
   end
 end
